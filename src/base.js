cd20dc614ca098e9948159df19833a60db71eb7c
ivotabako <3456952+ivotabako@users.noreply.github.com>
2019-06-08 03:07:15 +0200

    env

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
