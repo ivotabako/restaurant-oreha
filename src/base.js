0dd630887f0708fd776ea83b14f2464aa90fe7d0
ivotabako <3456952+ivotabako@users.noreply.github.com>
2019-06-08 02:21:42 +0200

    set env vars

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: ""
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
