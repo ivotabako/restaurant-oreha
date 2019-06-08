a18d33fe0a6e9cf92f2e33e7639885aef2abb138
ivotabako <3456952+ivotabako@users.noreply.github.com>
2019-06-08 03:45:48 +0200

    hardcode

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL
+  apiKey: "AIzaSyCXykJGh5MF9C9G86E2fUng9e0qDpewyCU",
+  authDomain: "restaurant-oreha.firebaseapp.com",
+  databaseURL: "https://restaurant-oreha.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
